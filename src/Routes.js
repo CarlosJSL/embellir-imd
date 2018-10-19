c858402cd7100590c275c7af4a38719c0f68e080
carlos <carlosjleonel@hotmail.com>
2018-10-18 21:53:49 -0300

    first commit

---
+++ src/Routes.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom'
+
+import Home from './containers/Home'
+import Auth from './containers/Auth'
+import PrivateRoute from './components/PrivateRoute'
+
+const Routes = (props) => {
+  return (  
+    <Router>
+      <Switch>
+        <PrivateRoute
+          path="/home"
+          component={Home}
+          authenticated={props.auth.allowed}
+        />
+
+        <Route
+          exact
+          path="/"
+          component={() => {
+            return props.auth.allowed
+              ? <Redirect to="/home" />
+              : <Auth />
+          }}
+        />
+      </Switch>
+    </Router>
+  )
+}
+
+export default connect(
+    state => ({ auth: state.auth })
+  )(Routes)
